59c30a7ce65e7b916e01fb0a5d2dae88b778d9af
Miguel <Miguel@192.168.1.6>
2020-04-16 10:55:16 -0300

    Modificações

--- TestIntegrationGit/src/testgiteclipse/TrainingJava.java
+++ TestIntegrationGit/src/testgiteclipse/TrainingJava.java
@@ -5,7 +5,7 @@ public class TrainingJava {
 	public static void main(String[] args) {
 		System.out.println("Olá mundo Git!");
 	// Alterado a partir do GitHub.	
-
+    // Alterado a partir do Eclipse.
 	}
 
 }
